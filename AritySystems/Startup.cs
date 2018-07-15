9a4e2d54bd54d3c323419593782e36e074372f00
Arity <arity.systems@gmail.com>
2018-07-15 17:23:23 +0530

    First Commit

---
+++ AritySystems/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AritySystems.Startup))]
+namespace AritySystems
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
