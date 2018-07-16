e941f4f51e97f9bdb2fd0319edcfd83f8ca3978c
Bhavesh Kachadiya <Irfan Kureshi@DESKTOP-JEJBI9G>
2018-07-17 02:12:20 +0530

    Make order page done for uer along with order liting

--- AritySystems/Controllers/OrderController.cs
+++ AritySystems/Controllers/OrderController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using AritySystems.Models;
+using AritySystems.Data;
+using System.Collections.Generic;
 
 namespace AritySystems.Controllers
 {
@@ -19,6 +21,147 @@ namespace AritySystems.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Order listing landing page
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult OrderList()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Get order details for customer
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetOrderList()
+        {
+            ArityEntities objDb = new ArityEntities();
+            var orderLst = (from order in objDb.Orders.ToList()
+                            select new
+                            {
+                                Id = order.Id,
+                                Prefix = order.Prefix,
+                                CreatedDate = order.CreatedDate,
+                                Status = order.Status,
+                                TotalItem = order.OrderLineItems.Sum(_ => _.Quantity),
+                                Total = order.OrderLineItems.Sum(_ => (_.DollarPurchasePrice * _.Quantity))
+                            }).ToList();
+            return Json(new { data = orderLst }, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Order details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult OrderDetail(int id)
+        {
+            ArityEntities objDb = new ArityEntities();
+            return View(objDb.Orders.Where(_ => _.Id == id).FirstOrDefault());
+        }
+        /// <summary>
+        /// Place order landing page
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult MakeOrder()
+        {
+            ArityEntities objDb = new ArityEntities();
+            ViewBag.Products = new SelectList(objDb.Products.Where(_ => _.Parent_Id == 0).ToList(), "Id", "English_Name");
+            return View();
+        }
+
+        /// <summary>
+        /// Get product for order
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="qty"></param>
+        /// <returns></returns>
+        public JsonResult AddProductToCart(int id, int qty)
+        {
+            ArityEntities objDb = new ArityEntities();
+            var product = (from pro in objDb.Products.ToList()
+                           where pro.Id == id
+                           select new Product
+                           {
+                               Dollar_Price = pro.Dollar_Price
+                           }).FirstOrDefault();
+            var items = objDb.Products.Where(_ => _.Id == id).Union(objDb.Products.Where(_ => _.Parent_Id == id)).ToList();
+            items.ForEach(_ => _.Quantity = qty);
+            var productList = (from lst in items
+                               select new Product
+                               {
+                                   English_Name = lst.English_Name,
+                                   Chinese_Name = lst.Chinese_Name,
+                                   Dollar_Price = lst.Dollar_Price,
+                                   RMB_Price = lst.RMB_Price,
+                                   Quantity = lst.Quantity,
+                                   Id = lst.Id,
+                                   Parent_Id = lst.Parent_Id
+                               }).ToList();
+            return Json(new { data = productList }, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Place order method
+        /// </summary>
+        /// <param name="fc"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult MakeOrder(FormCollection fc)
+        {
+            ArityEntities objDb = new ArityEntities();
+            List<KeyValuePair<int, int>> lineItem = new List<KeyValuePair<int, int>>();
+            ViewBag.Products = new SelectList(objDb.Products.Where(_ => _.Parent_Id == 0).ToList(), "Id", "English_Name");
+            if (fc != null)
+            {
+                try
+                {
+                    for (int i = 1; i <= 999999; i++)
+                    {
+                        if (fc["productId_" + i] == null && fc["qty_" + i] == null)
+                            break;
+                        lineItem.Add(new KeyValuePair<int, int>(Convert.ToInt32(fc["productId_" + i]), Convert.ToInt32(fc["qty_" + i])));
+                    }
+                    if (lineItem.Any())
+                    {
+                        var order = objDb.Orders.Add(new Order() { CustomerId = 1, CreatedDate = DateTime.Now, Prefix = "user1", Status = "1" });
+                        objDb.SaveChanges();
+                        foreach (var item in lineItem.Where(_ => _.Value > 0))
+                        {
+                            var prodcut = objDb.Products.Where(_ => _.Id == item.Key).FirstOrDefault();
+                            objDb.OrderLineItems.Add(new OrderLineItem() { OrderId = order.Id, ProductId = prodcut.Id, DollarPurchasePrice = prodcut.Dollar_Price, RMDPurchasePrice = prodcut.RMB_Price, Quantity = item.Value });
+                        }
+                        objDb.SaveChanges();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    TempData["Error"] = "Error occured while place your order. Please try again.";
+                    return View();
+                }
+            }
+            TempData["Success"] = "Order Placed. Thank you";
+            return View();
+        }
+
+        /// <summary>
+        /// Get Product details by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public JsonResult GetProductDetail(int id)
+        {
+            ArityEntities objDb = new ArityEntities();
+            var product = (from pro in objDb.Products.ToList()
+                           where pro.Id == id
+                           select new Product
+                           {
+                               Dollar_Price = pro.Dollar_Price
+                           }).FirstOrDefault();
+            return Json(product, JsonRequestBehavior.AllowGet);
         }
     }
 }
