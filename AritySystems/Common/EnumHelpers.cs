8f55d99b1d51d3890f02713b8b249900a1e16b99
Nirav Shah <nishah@apttus.com>
2018-07-18 21:52:14 +0530

    DB Changes and Product Changes

---
+++ AritySystems/Common/EnumHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AritySystems.Common
+{
+    public static class EnumHelpers
+    {
+        public enum Units {NOs,Ltr,Kg,gm }
+    }
+}
--- AritySystems/Controllers/ProductController.cs
+++ AritySystems/Controllers/ProductController.cs
@@ -9,13 +9,39 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using AritySystems.Models;
+using AritySystems.Data;
+using AritySystems.Common;
 
 namespace AritySystems.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     public class ProductController : Controller
     {
+        public ActionResult Create(int? Id)
+        {
+            try
+            {
+                Product product = new Product();
+                ArityEntities dataContext = new ArityEntities();
+                product = dataContext.Products.Where(x => x.Id == Id).FirstOrDefault();
+                ViewBag.productList = new SelectList(dataContext.Products.Where(x => x.Parent_Id == 0).ToList(), "Id", "English_Name");
+                return View(product);
+            }
 
-       
+            catch(Exception ex)
+            {
+                var exception = ex;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            ArityEntities dataContext = new ArityEntities();
+            dataContext.Products.Add(product);
+            dataContext.SaveChanges();
+            return View(product);
+        }
     }
 }
